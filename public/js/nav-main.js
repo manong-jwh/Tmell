b00300838d967a9fadeeeeb7ec7ac6aff1bc549e
码农小蒋 <1565151776@qq.com>
2020-10-20 16:08:55 +0800

    json-二级菜单

--- public/js/nav-main.js
+++ public/js/nav-main.js
@@ -1,4 +1,4 @@
-import { nav, navActive } from './style.js';
+import { nav, navActive, addXd } from './style.js';
 var $qycg = $('.qycg');
 var $three = $('.three');
 var $khfw = $('.khfw');
@@ -7,8 +7,18 @@ var $app = $('.app');
 var $down = $('.app .down');
 var $nav_active = $('.nav-side a');
 var $par = $('a.nav-active').parent();
-
 nav($qycg, $three);
 nav($khfw, $four);
 nav($app, $down);
 navActive($nav_active, $par);
+
+
+//导航吸顶
+let $nau = $('.nav-side');
+let $hid = $('.hidden');
+let $right = $('.aside-right');
+let $left = $('.aside-left');
+
+addXd($nau, $hid, $right, $left)
+
+//右
--- public/js/style.js
+++ public/js/style.js
@@ -22,4 +22,106 @@ function navActive(elem, elem1) {
 }
 
 
-export { nav, navActive }
+
+
+function addXd(elem, elem1, elem2, elem3) {
+    let mt = elem.offset().top;
+    let rt = elem2.offset().top;
+    let lt = elem3.offset().top;
+    let $ls = elem.children().eq(8).nextAll('li');
+    let lg = $('<i class="log">');
+    let go1 = $('<i class="go1"><i class="go">0</i></i>');
+    let me = $('<i class="me">');
+    let sear = $('<i class="sear">');
+    window.onscroll = () => {
+        var t = document.documentElement.scrollTop;
+        if (t >= mt) {
+            elem.css({
+                'margin': "0 auto",
+                'width': "1047px",
+                'line-height': '30px'
+            })
+            elem1.css({
+                'z-index': 99999,
+                'position': 'fixed',
+            })
+            $ls.each((i, elems) => {
+                $(elems).hide()
+            })
+            elem.children('li').each((i, elems) => {
+                $(elems).css({
+                    padding: '0 17px',
+                    "margin-top": '7px'
+                })
+            })
+            elem.prepend(lg);
+            elem.append(sear)
+            elem.append(me)
+            elem.append(go1)
+                // elem.append(go)
+        } else {
+            elem.css({
+                'margin-top': '110px',
+                'width': "1090px",
+                'line-height': '20px'
+            })
+
+            elem1.css({
+                'position': 'static',
+            })
+
+            $ls.each((i, elems) => {
+                $(elems).show()
+            })
+            elem.children('li').each((i, elems) => {
+                $(elems).css({
+                    padding: '0 26px',
+                    "margin-top": '0'
+                })
+            })
+            elem.children('li').eq(0).css({
+                padding: 0
+            })
+            elem.children('li').eq(9).css({
+                padding: 0
+            })
+            $('.log').remove();
+            $(".sear").remove()
+            $(".me").remove()
+            $(".go1").remove()
+        }
+
+        if (t >= rt - 50) {
+
+            elem2.css({
+                position: 'fixed',
+                top: '50px'
+            })
+
+        } else {
+            elem2.css({
+                position: 'absolute',
+                top: '620px'
+            })
+        }
+        if (t >= lt - 50) {
+
+            elem3.css({
+                position: 'fixed',
+                top: '50px'
+            })
+
+        } else {
+            elem3.css({
+                position: 'absolute',
+                top: '620px'
+            })
+        }
+    }
+}
+
+
+//左右广告
+
+
+export { nav, navActive, addXd }
